5e6cf8c5bcf833c03fb8505a80973ff75e137332
Christian Röpke <croepke@posteo.de>
2018-04-07 06:28:27 +0200

    finishing react router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts';
+import CreateContact from './CreateContact';
 import * as ContactsAPI from './utils/ContactsAPI';
+import { Route } from 'react-router-dom';
 
 class App extends Component {
   state = {
@@ -26,13 +28,32 @@ class App extends Component {
     ContactsAPI.remove(contact);
   }
 
+  createContact = (contact) => {
+    ContactsAPI.create(contact)
+      .then((contact) => {
+        this.setState((currentState) => ({
+          contacts: currentState.contacts.concat([contact])
+        }))
+      })
+  }
+
   render() {
     return (
       <div>
-        <ListContacts
-          contacts={this.state.contacts}
-          onDeleteContact={this.removeContact}
-        />
+        <Route exact path='/' render={() => (
+          <ListContacts
+            contacts={this.state.contacts}
+            onDeleteContact={this.removeContact}
+          />
+        )} />
+        <Route path='/create' render={({ history }) => (
+          <CreateContact
+            onCreateContact={(contact) => {
+              this.createContact(contact);
+              history.push('/');
+            }}
+          />
+        )} />
       </div>
     )
   }
